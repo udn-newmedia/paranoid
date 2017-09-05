3341730aff2c93b7624c8ba4909805f458cf36c2
Waiting7777 <waiting0819@msn.com>
2017-09-05 15:50:58 +0800

    fix hou banner

--- src/hou.js
+++ src/hou.js
@@ -40,7 +40,7 @@ class App extends Component{
                     <Head link="./hou.html" select="1"/> 
                     <Covers cover="hou-cover" title1="天生偏執狂" title2="小提琴家侯以嘉"/>
                     <HouContent />
-                    <Banner banner_id="sun-banner" text="當神秘東方遇上浪漫西方<br />侯以嘉  X 英國皇家愛樂管弦樂團" button_text="點我馬上購票" img_say="圖／侯以嘉提供 試聽音樂／聯合數位文創提供" link="https://tickets.udnfunlife.com/application/UTK02/UTK0201_.aspx?PRODUCT_ID=D0HOEFR5"/>
+                    <Banner banner_id="hou-banner" text="當神秘東方遇上浪漫西方<br />侯以嘉  X 英國皇家愛樂管弦樂團" button_text="點我馬上購票" img_say="圖／侯以嘉提供 試聽音樂／聯合數位文創提供" link="https://tickets.udnfunlife.com/application/UTK02/UTK0201_.aspx?PRODUCT_ID=D0HOEFR5"/>
                     <Relate exclude1={true} />
                     <FBComment />
                     <LogoBlock />
